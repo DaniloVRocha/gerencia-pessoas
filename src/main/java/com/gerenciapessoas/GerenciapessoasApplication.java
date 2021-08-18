31184181ab1cb3b56f84ed2727df1eea33a2edac
DaniloVRocha <danilorocha722@gmail.com>
2021-08-18 14:44:12 -0300

    Primeiro Commit

---
+++ src/main/java/com/gerenciapessoas/GerenciapessoasApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciapessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciapessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciapessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciapessoas/GerenciapessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciapessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciapessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
